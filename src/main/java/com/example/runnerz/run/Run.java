abbf61d460b936aa1045520050262f91c03b0dee
Jiayi Su <sujiayi317@gmail.com>
2024-08-19 11:39:03 -0400

    Add validation to the creation of Run object

--- src/main/java/com/example/runnerz/run/Run.java
+++ src/main/java/com/example/runnerz/run/Run.java
@@ -1,13 +1,23 @@
 package com.example.runnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Run(
         Integer id,
+        @NotEmpty
         String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
+        @Positive
         Integer miles,
         Location location
 ) {
+    public Run {
+        if (!completedOn.isAfter(startedOn)) {
+            throw new IllegalArgumentException("Started on date is after completed on date " + startedOn);
+        }
+    }
 }
