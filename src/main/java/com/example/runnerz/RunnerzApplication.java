84df703626d4a79ef84c7f622a018cc232c99dd2
Jiayi Su <sujiayi317@gmail.com>
2024-08-19 16:55:01 -0400

    Complete H2 database setup and batch upload

--- src/main/java/com/example/runnerz/RunnerzApplication.java
+++ src/main/java/com/example/runnerz/RunnerzApplication.java
@@ -2,6 +2,7 @@ package com.example.runnerz;
 
 import com.example.runnerz.run.Location;
 import com.example.runnerz.run.Run;
+import com.example.runnerz.run.RunRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -18,21 +19,24 @@ public class RunnerzApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzApplication.class, args);
 		log.info("Application started");
-
 	}
 
-	@Bean
-	CommandLineRunner commandLineRunner() {
-		return args -> {
-			log.info("Runner started");
-			Run run = new Run(1,
-					"First Run",
-					LocalDateTime.now(),
-					LocalDateTime.now().plusHours(1),
-					5,
-					Location.OUTDOOR);
-			log.info("Run" + run);
-		};
-	}
+//	@Bean
+//	CommandLineRunner commandLineRunner(RunRepository runRepository) {
+//		// delete data.sql in resources package:
+//		// INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+//		// VALUES (1, 'Morning Run', '2017-01-01 06:00:00', '2017-01-01 07:00:00', 5, 'OUTDOOR');
+//		return args -> {
+//			log.info("Runner started");
+//			Run run = new Run(1,
+//					"First Run",
+//					LocalDateTime.now(),
+//					LocalDateTime.now().plusHours(1),
+//					5,
+//					Location.OUTDOOR);
+//			log.info("Run" + run);
+//			runRepository.create(run);
+//		};
+//	}
 
 }
